28e09131a7fe3026bd0c59ac2d3edc72695b8866
kishoremanikandan <kishoremkishorem200@gmail.com>
2026-07-15 14:46:31 +0530

    Settings media code update

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -126,8 +126,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
-# MEDIA_URL='/media/'
+MEDIA_URL='/media/'
 # MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
